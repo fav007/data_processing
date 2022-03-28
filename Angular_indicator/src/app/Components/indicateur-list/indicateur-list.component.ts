18fc7f77fd16f789d34afd47234a4c85f5caf7ae
unknown <fenosoaranaivozanany@gmail.com>
2022-03-28 14:34:40 +0100

    up-0

--- Angular_indicator/src/app/Components/indicateur-list/indicateur-list.component.ts
+++ Angular_indicator/src/app/Components/indicateur-list/indicateur-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Indicateur } from 'src/app/models/Indicateur.model';
+import { Subscription } from 'rxjs';
+import { IndicatorService } from 'src/app/Services/indicator-service.service';
+
 
 @Component({
   selector: 'app-indicateur-list',
   templateUrl: './indicateur-list.component.html',
   styleUrls: ['./indicateur-list.component.scss']
 })
-export class IndicateurListComponent implements OnInit {
+export class IndicateurListComponent implements OnInit, OnDestroy {
+  
+  indicateurs: Indicateur[];
+  indicateurSubscription: Subscription;
 
-  constructor() { }
+  constructor(private indicateurService: IndicatorService) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+      this.indicateurSubscription = this.indicateurService.indicatorSubject.subscribe(
+        (indicateurs: Indicateur[]) => {
+          this.indicateurs = indicateurs;
+        }
+      );
+      this.indicateurService.emitIndicators();
   }
-
+  
+  ngOnDestroy(): void {
+    this.indicateurSubscription.unsubscribe();
+      
+  }
 }
